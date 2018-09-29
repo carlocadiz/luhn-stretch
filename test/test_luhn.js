4b396ae2884738c849c9b1fb09a26d0448752222
carlocadiz <carlocadiz71@gmail.com>
2018-09-29 04:14:29 +0000

    test file for luhn function

---
+++ test/test_luhn.js
@@ -0,0 +1,24 @@
+var assert = require("chai").assert;
+var luhn = require("../luhn");
+
+describe("Luhn validation function", function() {
+  it("should return true if number is valid", function() {
+    var number = 79927398713;
+    var result = luhn(number);
+    assert.isTrue(result);
+  });
+  it("should return true if number is valid", function() {
+    var number = 79927398712;
+    var result = luhn(number);
+    assert.isFalse(result);
+  });
+  it("should return true if number is valid", function() {
+    var number = 79927398719;
+    var result = luhn(number);
+    assert.isFalse(result);
+  });
+  it("should return true if number is valid", function() {
+    var number = 44825976127;
+    var result = luhn(number);
+    assert.isTrue(result);
+  });
