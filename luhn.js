67383d4d708c84c3819ea34a220a6e2223295d68
carlocadiz <carlocadiz71@gmail.com>
2018-09-29 04:18:41 +0000

    completed testing passed

--- luhn.js
+++ luhn.js
@@ -11,22 +11,10 @@ function luhn(number){ //var string = number.toString();
   newArray.forEach(function(element){
     sum = sum + Number(element);
   });
-  console.log((sum * 9) % 10, checkDigit);
   return ((sum * 9) % 10 === Number(checkDigit));
 
 
 }
 
 
-
-
-//console.log(sum);
-//console.log();
-//console.log(typeof(checkDigit));
-
-
-console.log(luhn(79927398713));
-console.log(luhn(79927398714));
-console.log(luhn(79927398719));
-
-console.log(luhn(44825976127));
+module.exports = luhn;
