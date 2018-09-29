854fc09d097226847925d93bec6b4f9140f99957
carlocadiz <carlocadiz71@gmail.com>
2018-09-29 04:04:30 +0000

    luhn functionality complete

--- luhn.js
+++ luhn.js
@@ -1,22 +1,32 @@
-function luhn(number){
-  //var string = number.toString();
+function luhn(number){ //var string = number.toString();
   var array = number.toString().split('');
-//  var times = parseInt(array.length / 2)
-
-
+  var sum = 0;
+  var checkDigit = 0;
   for (var i = array.length - 2; i > 0; i -= 2 ){
     array[i] = array[i] * 2;
   }
+  var newArray = array.toString().split(',').join('').split('');
 
+  checkDigit = newArray.pop();
+  newArray.forEach(function(element){
+    sum = sum + Number(element);
+  });
+  console.log((sum * 9) % 10, checkDigit);
+  return ((sum * 9) % 10 === Number(checkDigit));
 
-  console.log(array);
 
 }
 
 
 
 
+//console.log(sum);
+//console.log();
+//console.log(typeof(checkDigit));
 
 
+console.log(luhn(79927398713));
+console.log(luhn(79927398714));
+console.log(luhn(79927398719));
 
-luhn(79927398713);
+console.log(luhn(44825976127));
